2c0281538920635b4828b41df43c6c6c2bd6dedc
Ananya-1711 <ananyabanwanshi45@gmail.com>
2026-03-19 20:28:20 +0530

    Update modeControl.v

--- modeControl.v
+++ modeControl.v
@@ -1,25 +1,19 @@
 `timescale 1ns / 1ps
-
 module modeControl(
     input clock,
     input reset,
     input mode,
-
     input cand1_button,
     input cand2_button,
     input cand3_button,
     input cand4_button,
-
     input [7:0] cand1_vote_recvd,
     input [7:0] cand2_vote_recvd,
     input [7:0] cand3_vote_recvd,
-    input [7:0] cand4_vote_recvd,
-
+    input [7:0] cand4_vote_recvd
     output reg [7:0] leds
 );
-
 reg [31:0] counter;
-
 always @(posedge clock or posedge reset)
 begin
     if(reset) 
@@ -30,7 +24,6 @@ begin
 
     else 
     begin
-
         // -------- Voting Mode --------
         if(mode == 0) 
         begin
@@ -45,29 +38,21 @@ begin
                 counter <= 0;
             end
         end
-
         // -------- Result Mode --------
         else
         begin
             counter <= 0;
-
             if(cand1_button)
                 leds <= cand1_vote_recvd;
-
             else if(cand2_button)
                 leds <= cand2_vote_recvd;
-
             else if(cand3_button)
                 leds <= cand3_vote_recvd;
-
             else if(cand4_button)
                 leds <= cand4_vote_recvd;
-
             else
                 leds <= 8'b00000000;
         end
-
     end
 end
-
 endmodule
